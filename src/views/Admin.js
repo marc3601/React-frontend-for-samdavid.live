19e1e957a403a566624b40c790a835aebc331c82
Marcin Czaja <marcin.czaja@student.wsg.pl>
2021-02-25 22:05:17 +0100

    upload test

--- src/views/Admin.js
+++ src/views/Admin.js
@@ -1,8 +1,35 @@
-import React from "react";
-import { Container, Image } from "react-bootstrap";
+import React, { useState } from "react";
+import {
+  Container,
+  Image,
+  Card,
+  Col,
+  Form,
+  Button,
+  Row,
+  ProgressBar,
+} from "react-bootstrap";
 import plc from "../assets/devel.png";
-
+import axios from "axios";
 const Admin = () => {
+  const [file, setFile] = useState();
+  const [progress, setProgress] = useState();
+  const handleUpload = (e) => {
+    const data = new FormData();
+    data.append("song", file);
+    const config = {
+      onUploadProgress: (progressEvent) => {
+        let percentCompleted = Math.round(
+          (progressEvent.loaded * 100) / progressEvent.total
+        );
+        setProgress(percentCompleted);
+      },
+    };
+    axios
+      .post("https://api-music-test.herokuapp.com/data", data, config)
+      .then((res) => console.log(res));
+  };
+
   return (
     <Container className="text-center">
       <h2 className="display-3  mt-3 mb-3">CMS System</h2>
@@ -11,6 +38,45 @@ const Admin = () => {
         text or music.
       </p>
       <Image className="w-75" fluid src={plc} />
+      <Card className="mt-3 mb-3 w-100 mx-auto" lg={4}>
+        <Card.Title className="text-center mt-3 mb-3">
+          Upload form for testing purposes.
+        </Card.Title>
+        <Card.Body>
+          <Row>
+            <Col className="mx-auto">
+              <Form
+                className="text-center"
+                action="/data"
+                method="post"
+                encType="multipart/form-data"
+              >
+                <Form.Group>
+                  <Form.File
+                    type="file"
+                    name="song"
+                    id="song"
+                    label="Example file input"
+                    required
+                    onChange={(e) => {
+                      const file = e.target.files[0];
+                      setFile(file);
+                    }}
+                  />
+                  <ProgressBar className="mt-5" now={progress} />
+                </Form.Group>
+                <Button
+                  onClick={handleUpload}
+                  className="mt-4"
+                  variant="success"
+                >
+                  Upload
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
