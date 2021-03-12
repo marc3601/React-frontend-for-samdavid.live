660482c8d087708ff795cfef39b88f6fc110622e
Marcin Czaja <marcin.czaja@student.wsg.pl>
2021-03-12 23:20:11 +0100

    uploading to firebase in progress

--- src/views/Music.js
+++ src/views/Music.js
@@ -2,39 +2,55 @@ import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import ReactJkMusicPlayer from "react-jinke-music-player";
 import "react-jinke-music-player/assets/index.css";
-import axios from "axios";
+import { storage } from "../firebase";
 
 const Music = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const storageRef = storage.ref("songs");
+  const container = [];
   const downloadMusic = () => {
-    axios.get("https://api-music-test.herokuapp.com/data").then((res) => {
-      setData(res.data.files[0]);
-    });
+    storageRef
+      .listAll()
+      .then((result) => {
+        let name = "Default";
+        result.items.forEach(function (songRef) {
+          songRef.getMetadata().then((data) => name = data.name);
+          songRef
+            .getDownloadURL()
+            .then((url) => container.push({ name: name, musicSrc: url }));
+          
+        });
+      })
+      .finally(() => {
+        setData(container);
+      })
+      .catch((error) => {
+        // Handle any errors
+      });
   };
 
   useEffect(() => {
-    try {
-      downloadMusic();
-    } catch (error) {
-      console.log(error.message);
-    }
+    downloadMusic();
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
   }, []);
-
-  
-
 
   return (
     <Container>
       <h1 className="display-3 text-center text-dark">Music</h1>
 
-      <ReactJkMusicPlayer
-        audioLists={data}
-        theme="auto"
-        autoPlay={false}
-        toogleMode={false}
-        mode="full"
-        delete={false}
-      />
+      {!isLoading && (
+        <ReactJkMusicPlayer
+          audioLists={data}
+          theme="auto"
+          autoPlay={false}
+          toogleMode={false}
+          mode="full"
+          delete={false}
+        />
+      )}
     </Container>
   );
 };
