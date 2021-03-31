3953875de24dce34d95351c0a83cd1e59884ac0e
Marcin Czaja <marcin.czaja@student.wsg.pl>
2021-03-31 23:31:37 +0200

    panel functions

--- src/views/Admin.js
+++ src/views/Admin.js
@@ -1,5 +1,5 @@
-import { logRoles } from "@testing-library/dom";
 import React, { useState } from "react";
+import FileType from "file-type/browser";
 import {
   Container,
   Card,
@@ -18,88 +18,119 @@ const Admin = () => {
   const [message, setMessage] = useState("");
   const [progress, setProgress] = useState(0);
   const [duration, setDuration] = useState(null);
+  const [fileName, setFileName] = useState("");
+  const [fileType, setFileType] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
   const [category, setCategory] = useState("remixes");
-
   const handleUpload = (e) => {
-    if (file !== null) {
-      if (
-        file.type === "audio/mpeg" ||
-        file.type === "audio/ogg" ||
-        file.type === "audio/wav"
-      ) {
-        e.preventDefault();
-        const title = file.name.slice(0, file.name.length - 4);
-        setAlert(false);
-        setCompleted(false);
-        var metadata = {
-          name: title,
-          duration: duration,
-        };
-        const uploadTask = storageRef
-          .child(`${category}/${metadata.name}`)
-          .put(file, metadata);
+    if (fileType === "mp3" && duration) {
+      e.preventDefault();
+      setAlert(false);
+      setCompleted(false);
+      var metadata = {
+        name: fileName,
+        duration: duration,
+      };
+      const uploadTask = storageRef
+        .child(`${category}/${metadata.name}`)
+        .put(file, metadata);
 
-        setProgress(0);
-        uploadTask.on(
-          "state_changed",
-          (snapschot) => {
-            const progress = Math.round(
-              (snapschot.bytesTransferred / snapschot.totalBytes) * 100
-            );
-            setProgress(progress);
-          },
-          (err) => {
-            setProgress(0);
-            setAlert(true);
-            setMessage(err.message);
-          },
-          () => {
-            Promise.all([
-              uploadTask.snapshot.ref.getMetadata().then((data) => {
-                db.collection(category).doc(metadata.name).set({
-                  name: data.name,
-                  duration: duration,
-                });
-              }),
-
-              uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-                db.collection(category).doc(metadata.name).update({
-                  musicSrc: downloadURL,
-                });
-              }),
-            ])
-              .then(() => {
-                setCompleted(true);
-              })
-              .catch((err) => {
-                setProgress(0);
-                setAlert(true);
-                setMessage(err.message);
+      setProgress(0);
+      uploadTask.on(
+        "state_changed",
+        (snapschot) => {
+          setIsUploading(true);
+          const progress = Math.round(
+            (snapschot.bytesTransferred / snapschot.totalBytes) * 100
+          );
+          setProgress(progress);
+        },
+        (err) => {
+          setIsUploading(false);
+          setProgress(0);
+          setAlert(true);
+          setMessage(err.message);
+        },
+        () => {
+          Promise.all([
+            uploadTask.snapshot.ref.getMetadata().then((data) => {
+              db.collection(category).doc(metadata.name).set({
+                name: data.name,
+                duration: duration,
               });
-          }
-        );
-      }
+            }),
+
+            uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+              db.collection(category).doc(metadata.name).update({
+                musicSrc: downloadURL,
+              });
+            }),
+          ])
+            .then(() => {
+              setIsUploading(false);
+              setCompleted(true);
+              setDuration(null);
+              setFileName("");
+              setFileType(null);
+            })
+            .catch((err) => {
+              setIsUploading(false);
+              setProgress(0);
+              setAlert(true);
+              setMessage(err.message);
+            });
+        }
+      );
     } else {
-      if (file == null || file.type !== "audio/mpeg") {
+      if (file == null || fileType !== "mp3") {
         setAlert(true);
-        setMessage("Choose audio file before uploading.");
+        setMessage("Please choose mp3 file.");
+      } else if (duration === null) {
+        setAlert(true);
+        setMessage("Wait a second... Duration data is being loaded.");
       }
     }
   };
 
+  const checkFileType = (input) => {
+    const url = URL.createObjectURL(input);
+    const title = input.name.slice(0, input.name.length - 4);
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.blob();
+        } else {
+          setFileType(null);
+        }
+      })
+      .then(async (blob) => {
+        const type = await FileType.fromBlob(blob);
+        setFileType(type.ext);
+        if (type.ext === "mp3") {
+          setFileName(title);
+        } else setFileName("");
+      })
+      .catch((e) => {
+        setFileType(null);
+        console.log(
+          "There has been a problem with your fetch operation: ",
+          e.message
+        );
+      });
+  };
+
   const getFileDuration = (input) => {
     const reader = new FileReader();
-    reader.onload = function (event) {
+    reader.onload = (e) => {
       const audioContext = new (window.AudioContext ||
         window.webkitAudioContext)();
-      audioContext.decodeAudioData(event.target.result, function (buffer) {
+      audioContext.decodeAudioData(e.target.result, (buffer) => {
         const duration = buffer.duration;
         setDuration(convertAudioDuration(duration));
-        return duration;
       });
     };
-    reader.onerror = function (event) {
-      console.error("An error ocurred reading the file: ", event);
+    reader.onerror = (e) => {
+      console.error("An error ocurred reading the file: ", e);
     };
 
     reader.readAsArrayBuffer(input);
@@ -135,68 +166,101 @@ const Admin = () => {
     <Container className="text-center">
       <h2 className="display-4  mt-3 mb-4">Content management</h2>
       <p className="lead">Music upload system</p>
-      <Card className="mt-5 mb-3 ">
-        <Card.Title className="text-center mt-3 mb-3" md={4}>
-          Choose category and file to upload.
-        </Card.Title>
-        <Card.Body>
-          <Row>
-            <Col className="mx-auto">
-              <Form
-                className="text-center"
-                action="/data"
-                method="post"
-                encType="multipart/form-data"
-              >
-                <Form.Group>
-                  <Form.Control
-                    as="select"
-                    className="mr-sm-2"
-                    label="Choose category."
-                    id="inlineFormCustomSelect"
-                    custom
-                    onChange={(e) => setUserChoice(e.target.value)}
+      <Row className="d-flex justify-content-center">
+        <Col lg={8}>
+          <Card className="mt-5 shadow p-3 mb-5 bg-white rounded">
+            <Card.Title className="text-center mt-3 mb-3" md={4}>
+              Choose category and file to upload.
+            </Card.Title>
+            <Card.Body>
+              <Row>
+                <Col className="mx-auto">
+                  <Form
+                    className="text-center"
+                    action="/data"
+                    method="post"
+                    encType="multipart/form-data"
                   >
-                    <option value="0">Remixes</option>
-                    <option value="1">Dj sets</option>
-                    <option value="2">Original music</option>
-                    <option value="3">Projects</option>
-                  </Form.Control>
-                  <Form.File
-                    type="file"
-                    name="song"
-                    id="song"
-                    label="Choose audio file to upload."
-                    required
-                    className="mt-4"
-                    onChange={(e) => {
-                      const file = e.target.files[0];
-                      setFile(file);
-                      if (file.type !== null) {
-                        getFileDuration(file);
-                      }
-                    }}
-                  />
-                  <ProgressBar className="mt-5" now={progress} />
-                </Form.Group>
-                <Button
-                  onClick={handleUpload}
-                  className="mt-4"
-                  variant="success"
-                >
-                  Upload
-                </Button>
-                <Alert className="mt-3" variant="danger" show={alert}>
-                  {message}
-                </Alert>
-                <Alert className="mt-3" variant="success" show={completed}>
-                  Upload completed
-                </Alert>
-              </Form>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+                    <Form.Group>
+                      <Form.Control
+                        as="select"
+                        className="mr-sm-2"
+                        label="Choose category."
+                        id="inlineFormCustomSelect"
+                        custom
+                        onChange={(e) => setUserChoice(e.target.value)}
+                      >
+                        <option value="0">Remixes</option>
+                        <option value="1">Dj sets</option>
+                        <option value="2">Original music</option>
+                        <option value="3">Projects</option>
+                      </Form.Control>
+                      <Form.File
+                        type="file"
+                        name="song"
+                        id="song"
+                        label="Choose audio file to upload."
+                        required
+                        className="mt-4"
+                        onChange={(e) => {
+                          const file = e.target.files[0];
+                          setFile(file);
+                          if (file !== null || file !== undefined) {
+                            getFileDuration(file);
+                            checkFileType(file);
+                          }
+                        }}
+                      />
+                      <ProgressBar className="mt-5" now={progress} />
+                    </Form.Group>
+                    <Button
+                      onClick={!isUploading ? handleUpload : undefined}
+                      className="mt-4"
+                      variant="success"
+                    >
+                      Upload
+                    </Button>
+                    <Alert className="mt-3" variant="danger" show={alert}>
+                      {message}
+                    </Alert>
+                    <Alert className="mt-3" variant="success" show={completed}>
+                      Upload completed
+                    </Alert>
+                  </Form>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row className="d-flex justify-content-center">
+        <Col lg={6}>
+          <Card className="mt-5 shadow p-3 mb-5 bg-white rounded">
+            <Card.Title className="text-center mt-3 mb-3">
+              Detected metadata.
+            </Card.Title>
+            <Card.Body>
+              <Row>
+                <Col className="text-left">
+                  <p className="lead">
+                    <span className="font-weight-bold">Title: </span>
+                    {fileName}
+                  </p>
+                  <p className="lead">
+                    <span className="font-weight-bold">Duration: </span>
+                    {duration}
+                  </p>
+                  <p className="lead">
+                    <span className="font-weight-bold">File type: </span>
+                    {fileType}
+                  </p>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
