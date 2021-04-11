e6674ca4e0cd55887f4372b1c6a891db2cb72292
Marcin Czaja <marcin.czaja@student.wsg.pl>
2021-04-11 11:58:36 +0200

    less videos

--- src/views/galleryCategories/Videos.js
+++ src/views/galleryCategories/Videos.js
@@ -5,11 +5,8 @@ import 'plyr-react/dist/plyr.css';
 import styled from 'styled-components';
 import vid1 from '../../assets/vid1.MP4';
 import vid2 from '../../assets/vid2.MP4';
-import vid3 from '../../assets/vid3.MP4';
-import vid4 from '../../assets/vid4.MP4';
-import vid5 from '../../assets/vid5.MP4';
-import vid6 from '../../assets/vid6.MP4';
-const arr1 = [vid5, vid6, vid1, vid2, vid3, vid4];
+
+const arr1 = [vid1, vid2];
 
 const Videos = () => {
   let source1 = [];
