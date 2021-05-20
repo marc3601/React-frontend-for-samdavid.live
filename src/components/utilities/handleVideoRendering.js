5d908fd647a2742194fd6da276ff0a32ecbd98ed
Marcin Czaja <marcin.czaja@student.wsg.pl>
2021-05-20 17:32:52 +0200

    Quick video upload

--- src/components/utilities/handleVideoRendering.js
+++ src/components/utilities/handleVideoRendering.js
@@ -11,24 +11,24 @@ export const handleVideoRendering = (input) => {
   const firstArrLenght = Math.round(lnght / 2);
   newArr = input.slice(0, firstArrLenght);
   newAr2 = input.slice(firstArrLenght, input.length);
-  const controls = [
-    'play-large', // The large play button in the center
-    //'restart', // Restart playback
-    'rewind', // Rewind by the seek time (default 10 seconds)
-    'play', // Play/pause playback
-    'fast-forward', // Fast forward by the seek time (default 10 seconds)
-    'progress', // The progress bar and scrubber for playback and buffering
-    'current-time', // The current time of playback
-    'duration', // The full duration of the media
-    'mute', // Toggle mute
-    'volume', // Volume control
-    'captions', // Toggle captions
-    'settings', // Settings menu
-    'pip', // Picture-in-picture (currently Safari only)
-    'airplay', // Airplay (currently Safari only)
-    'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
-    'fullscreen', // Toggle fullscreen
-  ];
+  // const controls = [
+  //   'play-large', // The large play button in the center
+  //   //'restart', // Restart playback
+  //   'rewind', // Rewind by the seek time (default 10 seconds)
+  //   'play', // Play/pause playback
+  //   'fast-forward', // Fast forward by the seek time (default 10 seconds)
+  //   'progress', // The progress bar and scrubber for playback and buffering
+  //   'current-time', // The current time of playback
+  //   'duration', // The full duration of the media
+  //   'mute', // Toggle mute
+  //   'volume', // Volume control
+  //   'captions', // Toggle captions
+  //   'settings', // Settings menu
+  //   'pip', // Picture-in-picture (currently Safari only)
+  //   'airplay', // Airplay (currently Safari only)
+  //   'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+  //   'fullscreen', // Toggle fullscreen
+  // ];
   newArr.forEach((item) => {
     source1.push({
       type: 'video',
@@ -57,7 +57,13 @@ export const handleVideoRendering = (input) => {
         {source1.length > 0 &&
           source1.map((vid, i) => (
             <VideoContainer key={i}>
-              <Plyr source={vid} options={controls} />
+              {/* <Plyr source={vid} options={controls} /> */}
+              <video
+                src={vid.sources[0].src}
+                style={{maxWidth: '100%'}}
+                playsInline
+                controls
+              />
             </VideoContainer>
           ))}
       </Col>
@@ -65,7 +71,14 @@ export const handleVideoRendering = (input) => {
         {source2.length > 0 &&
           source2.map((vid, i) => (
             <VideoContainer key={i}>
-              <Plyr source={vid} options={controls} />
+              {/* <Plyr source={vid} options={controls} /> */}
+              <video
+                src={vid.sources[0].src}
+                style={{maxWidth: '100%'}}
+                playsInline
+                preload="metadata"
+                controls
+              />
             </VideoContainer>
           ))}
       </Col>
@@ -74,6 +87,8 @@ export const handleVideoRendering = (input) => {
 };
 
 const VideoContainer = styled.div`
-  box-shadow: 0px 0px 24px -6px rgba(0, 0, 0, 0.75);
+  /* box-shadow: 0px 0px 24px -6px rgba(0, 0, 0, 0.75); */
   margin-bottom: 1rem;
+  display: flex;
+  justify-content: center;
 `;
